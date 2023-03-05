a31f5a1a6dbb64d07c004625a74633a330358030
DESKTOP-3UB9VKS\ahmad <ahmad.sarraj.772@gmail.com>
2023-03-05 16:53:25 +0200

    03052023

--- src/dumy_data/category.js
+++ src/dumy_data/category.js
@@ -1,7 +1,7 @@
 export const categories = [
-    {id: 1, name: 'Vehicules & Mototrs', url: "../../public/assets/icons8-hennessey-venom-90.png"},
-    {id: 2, name: 'Special', url: "../../public/assets/icons8-paste-special-90.png"},
-    {id: 3, name: 'Applicances and Furniture', url: "../../public/assets/icons8-sofa-90.png"},
-    {id: 4, name: 'Fashion', url: "../../public/assets/icons8-hanger-90.png"},
-    {id: 5, name: 'category1', url: "../../public/assets/icons8-department-90.png"},
+    {id: 1, name: 'Vehicules & Mototrs', url: "/public/assets/icons8-hennessey-venom-90.png"},
+    {id: 2, name: 'Special', url: "/public/assets/icons8-paste-special-90.png"},
+    {id: 3, name: 'Applicances and Furniture', url: "/public/assets/icons8-sofa-90.png"},
+    {id: 4, name: 'Fashion', url: "/public/assets/icons8-hanger-90.png"},
+    {id: 5, name: 'category1', url: "/public/assets/icons8-department-90.png"},
 ]
